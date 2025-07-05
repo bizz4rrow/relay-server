5ed2069184d4f3366b8acc262a958f4a7d02b9bf
bizz4rrow <144330469+bizz4rrow@users.noreply.github.com>
2025-07-05 17:54:27 +0200

    Update server.py

--- server.py
+++ server.py
@@ -19,5 +19,5 @@ async def websocket_endpoint(websocket: WebSocket):
         connected_clients.remove(websocket)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("server:app", host="0.0.0.0", port=port)
